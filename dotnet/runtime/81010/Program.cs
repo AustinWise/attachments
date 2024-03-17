6bee173c5c71075d9b717afc60175c8c4c49a7bf
Austin Wise <AustinWise@gmail.com>
2024-03-17 09:20:42 -0700

    81010

---
+++ dotnet/runtime/81010/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+static class Program
+{
+    static void FillStack(byte value)
+    {
+        Span<byte> bytes = stackalloc byte[1024 * 16];
+        bytes.Fill(value);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Attach to PID {Process.GetCurrentProcess().Id} and press enter.");
+        Console.ReadLine();
+        AppDomain.CurrentDomain.UnhandledException += (_,_) => FillStack(0xff);
+        throw new Exception();
+    }
+}
---
+++ dotnet/runtime/81010/README.md
@@ -0,0 +1,20 @@
+
+This is a reproduction program for https://github.com/dotnet/runtime/pull/81010 .
+
+Run with:
+
+```batch
+run.cmd
+```
+
+Then attach with Visual Studio and Press Enter. When it breaks on the unhandled exception,
+the call stack should look garbled:
+
+![](screenshot.png)
+
+# Versions tested with
+
+* .NET SDK 7.0.102
+* .NET 7.0.2
+* Windows 11  - 10.0.22621.1105
+* Visual Studio 17.4.4
